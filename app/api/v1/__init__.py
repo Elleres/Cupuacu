9217402891e911f834597559db5a1ee209ed00c7
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-06-04 08:39:24 -0300

    build: alterar docker-compose e mudar organizacao do codigo

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from api.v1.user import router as user_router
+
+
+router = APIRouter()
+
+router.include_router(user_router)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,6 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
-from api.v1.user import router as user_router
-
+from api.v1 import router
 app = FastAPI()
 
-app.include_router(user_router)
-
-
-@app.get("/")
-def read_root():
-    return {"message": "API FastAPI rodando no Docker com sucesso 🚀"}
+app.include_router(router)
