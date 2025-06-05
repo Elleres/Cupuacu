51f279dd24a927faac9ba931aa018d3b02877018
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-06-05 12:40:18 -0300

    feat: adicionar log + adicionar login de usuario

--- app/models/user.py
+++ app/models/user.py
@@ -5,9 +5,13 @@ from sqlmodel import Field, SQLModel
 
 class User(SQLModel, table=True):
     __tablename__ = "user"
-    __table_args__ = (UniqueConstraint("email", name="uq_user_email"),)
+    __table_args__ = (
+        UniqueConstraint("email", name="uq_user_email"),
+        UniqueConstraint("username", name="uq_user_username"),
+    )
 
     id: UUID = Field(default_factory=uuid4, primary_key=True)
+    username: str = Field(nullable=False, max_length=255)
     name: str = Field(nullable=False, max_length=255)
     email: str = Field(nullable=False, max_length=255)
     password: str = Field(nullable=False, max_length=255)
