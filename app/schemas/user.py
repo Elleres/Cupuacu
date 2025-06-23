ea59ed0307dfe9e0721e80952847bde239f91b6a
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-06-23 19:15:03 -0300

    feat: adicionar status + type na tabela user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,22 +1,26 @@
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+
+from const.enum import UserStatusType, UserType
 
 
 class UserResponse(BaseModel):
     id: UUID
-    name: str
-    email: str
-    username: str
+    name: str = Field(..., examples=["John Doe", "Joao Fernando"])
+    email: str = Field(..., examples=["exemplo@gmail.com"])
+    username: str = Field(..., examples=["john_doe"])
+    status: UserStatusType
+    type: UserType
 
     model_config = ConfigDict(from_attributes=True)
 
 class UserCreate(BaseModel):
-    name: str
-    email: str
+    name: str = Field(..., examples=["John Doe", "Joao Fernando"])
+    email: str = Field(..., examples=["exemplo@gmail.com"])
     password: str
-    username: str
-
+    username: str = Field(..., examples=["john_doe"])
+    type: UserType = Field(..., examples=[UserType.gerente_laboratorio])
 
 class UserLogin(BaseModel):
     username: str
