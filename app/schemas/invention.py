fcd896bc8010afb6274e335b46b4762fe8e45e0f
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-06-24 15:50:17 -0300

    feat: create invention table + create_invetion route

---
+++ app/schemas/invention.py
@@ -0,0 +1,34 @@
+from datetime import date
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from const.enum import InventionType, InventionStatusType
+
+
+class InventionCreate(BaseModel):
+
+    titulo: str = Field(..., example="Cadeira de Rodas Infantil Acessível")
+    descricao: str = Field(
+        ...,
+        example=(
+            "Trata-se de uma cadeira de rodas infantil, que atende crianças com deficiência e "
+            "que é capaz de proporcionar mais independência em seu dia a dia. É de fabricação "
+            "acessível, pode ser reutilizada, seu material é de baixo custo (MDF) e impressão 3D, "
+            "apenas as rodinhas de silicone são compradas por fora. Todo o trabalho é feito no "
+            "próprio laboratório do Ipê lab. A Mariana, aluna da faculdade de Design de Moda (FAV/UFG), "
+            "também participa criando as almofadas e os estofados. É uma tecnologia que proporciona "
+            "acessibilidade para crianças."
+        )
+    )
+    situacao: InventionStatusType = Field(..., example=InventionStatusType.concedido_registrado)
+    trl: int = Field(..., example=5, description="Nível de Maturidade Tecnológica (1 a 9)")
+    type: InventionType = Field(..., example=InventionType.patente_de_invencao)
+    data_submissao: date = Field(..., example=date.today().isoformat(),
+                                 description="Data de submissão no formato YYYY-MM-DD")  # Exemplo no formato ISO 8601
+
+
+class InventionResponse(InventionCreate):
+    id: UUID = Field(..., example="a1b2c3d4-e5f6-7890-1234-567890abcdef")
+
+    model_config = ConfigDict(from_attributes=True)
