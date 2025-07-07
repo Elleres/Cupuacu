41bd544e95a12eb33220d128b7007ee262de6e60
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-07-07 10:07:42 -0300

    feat: add rotas para deletar as invencoes

--- app/repositories/invention_repositories.py
+++ app/repositories/invention_repositories.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.invention import Invention
@@ -17,3 +19,29 @@ async def create_invention(
     await db.refresh(db_user)
 
     return db_user.model_dump()
+
+
+async def get_invention(
+        db: AsyncSession,
+        invention_id: UUID
+):
+    db_invention = await db.get(Invention, invention_id)
+
+    if not db_invention:
+        return None
+
+    return db_invention
+
+async def delete_invention(
+        db: AsyncSession,
+        invention_id: UUID
+):
+    db_invention = await db.get(Invention, invention_id)
+
+    if not db_invention:
+        return None
+
+    await db.delete(db_invention)
+    await db.commit()
+
+    return 1
