99e33d42d61f09e98615c377c365b2ed97f4fae8
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-07-08 00:23:50 -0300

    fix: token url on prod

--- app/main.py
+++ app/main.py
@@ -7,15 +7,17 @@ from fastapi.security import OAuth2PasswordBearer
 from api.v1 import router
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 if ENVIRONMENT == "prod":
     ROOT_PATH_URL = os.getenv("ROOT_PATH_URL", "")
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl=ROOT_PATH_URL + "/token")
     app = FastAPI(
         swagger_ui_parameters={"docExpansion": "none"},
         root_path=ROOT_PATH_URL
     )
 else:
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
     app = FastAPI(
         swagger_ui_parameters={"docExpansion": "none"}
     )
