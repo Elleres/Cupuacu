ea756578fea30930ccb5cf793dde55bbe71e5223
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-07-18 17:57:39 -0300

    perf: remove reload from uvicorn command prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "Banco pronto, rodando migrations..."
 alembic upgrade head
 
 if [ "$ENVIRONMENT" = "prod" ]; then
-  exec uvicorn main:app --host 0.0.0.0 --port 8000 --log-config utils/logging.yml --reload --root-path /api/labes
+  exec uvicorn main:app --host 0.0.0.0 --port 8000 --log-config utils/logging.yml --root-path /api/labes
 else
   exec uvicorn main:app --host 0.0.0.0 --port 8000 --log-config utils/logging.yml --reload
 fi
