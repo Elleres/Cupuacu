f5c1e9d204d388147d2a0cb8251d626b65c9ec35
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2025-07-18 22:23:49 -0300

    feat: filtro para tickets

--- app/schemas/ticket.py
+++ app/schemas/ticket.py
@@ -8,11 +8,9 @@ from const.enum import TicketStatusType
 
 
 class TicketCreateRequest(BaseModel):
-    id_tecnologia_alvo: UUID = Field(..., example="c1b3c3d4-e5f6-7890-1234-567890cdddef")
+    id_tecnologia_alvo: Optional[UUID] = Field(example="c1b3c3d4-e5f6-7890-1234-567890cdddef")
     nome_do_projeto: str = Field(..., example="ENTRE - Ferramenta de modelagem de curriculos")
     status: TicketStatusType = TicketStatusType.open
-    criado_em: datetime = datetime.now()
-    atualizado_em: datetime = datetime.now()
 
 class TicketCreate(TicketCreateRequest):
     id_user: UUID = Field(..., example="a1b2c3d4-e5f6-7890-1234-567890cdddef")
@@ -21,9 +19,3 @@ class TicketResponse(TicketCreate):
     id: UUID = Field(..., example="a1b2c3d4-e5f6-7890-1234-567890abcdef")
 
     model_config = ConfigDict(from_attributes=True)
-
-class TicketSearchParams(BaseModel):
-    start: int = Field(default=0, ge=0)
-    limit: int = Field(default=20, ge=1)
-    id_tecnologia_alvo: Optional[UUID]
-    status: Optional[List[TicketStatusType]] = Field(None)
